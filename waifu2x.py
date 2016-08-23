8d267fec340d6f06554a16c6dbfb5dcf2a771f45
Joshua Li <jrl5696@psu.edu>
2016-08-23 14:38:32 -0400

    removed uuid, output filename now reflects user selected options

--- waifu2x.py
+++ waifu2x.py
@@ -10,10 +10,8 @@ Python module to make POST requests to the waifu2x image enhancement API.
 import os
 import sys
 import requests
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlsplit
 from io import BytesIO
-from uuid import uuid4
-# TODO swap out uuid for orginal filename w/ options appended
 
 POST_URL = 'http://waifu2x.udp.jp/api'
 
@@ -24,16 +22,18 @@ def _get_file(img_loc):
         r = requests.get(img_loc)
         if r.status_code == 200:
             if not img_loc.lower().endswith(('.jpg', '.png')):
-                raise RuntimeError('URL is not a jpg/png image.')
+                raise RuntimeError('URL does not link to a jpg/png image.')
             file = BytesIO()
             for chunk in r.iter_content(1024):
                 file.write(chunk)
             file.seek(0)
-            return {'file': file.read()}
+            fn = os.path.splitext(os.path.basename(urlsplit(img_loc).path))[0]
+            return ({'file': file.read()}, fn)
         else:
             raise RuntimeError('URL access failure [{}].'.format(r.status_code))
     else:
-        return {'file': open(img_loc, 'rb')}
+        fn = os.path.splitext(os.path.basename(img_loc))[0]
+        return ({'file': open(img_loc, 'rb')}, fn)
 
 
 def _get_options(style, noise, scale):
@@ -48,40 +48,43 @@ def _get_options(style, noise, scale):
     if scale not in (-1, 1, 2):
         scale = -1
     options = {'style': style, 'noise': noise, 'scale': scale}
+    op_str = "_waifu2x"
     for o in options:
-        print('{}: {}'.format(o, options[o]))
-    return options
+        op_str += "_{}{}".format(o, options[o])
+    return options, op_str
 
-
-# TODO support returning PIL objects in addition to local download
-# or just make local download a separate function
-# TODO allow customize local path to save to
+# TODO save image to same local path
 # TODO add option to stop output on all functions (for library output functionality)
-def process(img_loc, style='art', noise=3, scale=-1):
+def process_dl(img_loc, style='art', noise=3, scale=-1):
     # img_loc can be either a URL or a local filepath
     try:
-        file = _get_file(img_loc)
+        file, name = _get_file(img_loc)
     except RuntimeError as e:
         print("Error retrieving original image file: {}".format(e))
         return -1
-    options = _get_options(style, noise, scale)
+    options, op_str = _get_options(style, noise, scale)
     print('Making request to waifu2x...')
     r = requests.post(POST_URL, files=file, data=options, stream=True)
     if r.status_code == 200:
         print('Request successful. Downloading...')
-        out_fn, total, progress = str(uuid4()) + '.png', len(r.content), 0
-        with open(out_fn, 'wb') as f:
+        result_fn = ''.join((name, op_str, '.png'))
+        total, progress = len(r.content), 0
+        with open(result_fn, 'wb') as f:
             for chunk in r.iter_content(1024*64):
                 f.write(chunk)
                 progress += len(chunk)
                 print('{} / {} bytes downloaded'.format(progress, total))
-        print('Download complete. Saved to {}'.format(os.path.join(os.getcwd(), out_fn)))
+        print('Download complete. Saved to {}'.format(os.path.join(os.getcwd(), result_fn)))
     else:
         print(r.text)
 
+# TODO support returning PIL objects in addition to local download
+def process_pil():
+    pass
+
 def main():
     usage = '[image url/path] [style (art/photo)] [noise (-1 - 3)] [scale (-1, 1, 2)]'
-    process(*tuple(input(usage + '\n').strip().split()))
+    process_dl(*tuple(input(usage + '\n').strip().split()))
     return 0
 
 if __name__ == "__main__":
